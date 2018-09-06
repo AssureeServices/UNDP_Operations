bce9ea057695411aa6491ff6147db0cf44e1f62d
Irfan <kirfan@assureservices.com>
2018-09-06 21:03:17 +0530

    UNDP_Operations
    UNDP_Operations

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+var open = require('open');
+
+var app = express();
+app.use(express.static(__dirname));
+
+
+app.use("/main", function(req, res){  
+	//res.send('Hello World!');
+  res.sendFile(__dirname + "/main.html");
+});
+
+app.listen(8000, function(){
+  console.log('listening on port 8000');
+  open('http://localhost:8000');
+
+});
